793ba7a5d1bd0802d2bc64d8bc9d64f7982241d7
Yasushi Asahi <zero.asahi@karabiner.tech>
2021-03-10 18:15:18 +0900

    serverless

---
+++ pages/time2.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home({time}) {
+  return (
+    <>
+    <h1>It's {time}</h1>
+    <hr/>
+    <Link href="/"><a>top</a></Link><br/>
+    <Link href="/time"><a>time</a></Link><br/>
+    <Link href="/time2"><a>time2</a></Link>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+
+  const date = new Date()
+  const y = date.getFullYear()
+  const d = date.getDate()
+  const h = date.getHours()
+  const m = date.getMinutes()
+  const s = date.getSeconds().toString()
+  const time = `${y}/${m}/${d} ${h}:${m}:${s.padStart(2,"0")}`
+
+  return {
+    props: {time}, // will be passed to the page component as props
+    revalidate: 1,
+  }
+}
